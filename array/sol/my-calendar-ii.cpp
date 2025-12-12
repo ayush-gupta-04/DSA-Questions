e28541d13d679006795dfff7368fc5e6eae83e2b
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2025-12-12 19:20:23 +0530

    Update my-calendar-ii.cpp

--- array/sol/my-calendar-ii.cpp
+++ array/sol/my-calendar-ii.cpp
@@ -1,3 +1,4 @@
+// ------------------------------------------------ BRUTE FORCE ---------------------------------------------------------------------
 // Calendar - I
 // -> for an event {s,e} .... 
 //    if this event intersects with any of the other interval .. then we cannot add this event.
@@ -59,8 +60,49 @@ public:
 
 };
 
-/**
- * Your MyCalendarTwo object will be instantiated and called as such:
- * MyCalendarTwo* obj = new MyCalendarTwo();
- * bool param_1 = obj->book(startTime,endTime);
- */
+
+// ------------------------------------------------ LINE SWEEP ALGORITHM ---------------------------------------------------------------------
+
+
+// The Line Sweep algorithm works by marking when bookings start and end.
+// For each booking (start, end), we mark the start point by increasing its count by 1 
+//     (indicating a booking begins), and we mark the end point by decreasing its count by 1 
+//     (indicating a booking ends).
+// These marks are stored in a map, which keeps track of the number of bookings starting or ending at each point.
+// Once all bookings are processed, we compute the prefix sum over the map
+// The prefix sum at any point tells us how many active bookings overlap at that moment. 
+// If the sum at any point exceeds 2, it means we have a triple booking. 
+
+class MyCalendarTwo {
+public:
+    // Store the number of bookings at each point.
+    map<int, int> bookingCount;
+
+    MyCalendarTwo() { }
+
+    bool book(int start, int end) {
+        // Increase and decrease the booking count at the start and end
+        // respectively.
+        bookingCount[start]++;
+        bookingCount[end]--;
+
+        int overlappedBooking = 0;
+        // Find the prefix sum.
+        for (pair<int, int> bookings : bookingCount) {
+            overlappedBooking += bookings.second;
+
+            // If the number of bookings is more than 2, return false.
+            // Also roll back the counts for this booking as we won't add it.
+            if (overlappedBooking > 2) {
+                bookingCount[start]--;
+                bookingCount[end]++;
+
+                // Remove the entries from the map to avoid unnecessary
+                // iteration.
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
