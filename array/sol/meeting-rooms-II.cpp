e973e2c641967888a8f2c09772d94869ce9e4b70
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2025-12-13 22:28:35 +0530

    Update meeting-rooms-II.cpp

--- array/sol/meeting-rooms-II.cpp
+++ array/sol/meeting-rooms-II.cpp
@@ -1,4 +1,6 @@
 // we just need to find the maximum number of active rooms at a given time.
+// we will sort the interval.
+// we will sweep though the time .. and calc the maximum active rooms , and calc the max.
 class Solution {
 public:
     int minMeetingRooms(vector<int>& start, vector<int>& end) {
@@ -30,3 +32,39 @@ public:
         return maxRooms;
     }
 };
+
+
+
+// -------------------------------------------------- 2-Pointers -------------------------------------------------------
+
+// what we are concerned of :
+// 1. number of active meetings at a given point of time....i.e max meetings intersection.
+// 2. As we are sweeping through time .. we don't care about the pair (startTime , endTime).
+//    all we care about is that ... (it must be sorted , we should know is it start or end.).
+// 3. We can sort StartTime , EndTime separately .... and we will have 2 pointers.
+
+class Solution {
+public:
+    int minMeetingRooms(vector<int>& start, vector<int>& end) {
+        int n = start.size();
+        sort(start.begin() , start.end());
+        sort(end.begin() , end.end());
+        
+        int i = 0;
+        int j = 0;
+        int cnt = 0;
+        int ma = 0;
+        while(i < n && j < n){
+            if(start[i] < end[j]){
+                cnt++;
+                i++;
+            }else if(start[i] >= end[j]){    // if start == end .. i will process end first.
+                cnt--;
+                j++;
+            }
+            ma = max(ma , cnt);
+        }
+        return ma;
+    }
+};
+
