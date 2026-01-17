694e66da175b0bc75d1ed26500e489bc1c694c60
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-01-17 10:46:07 +0530

    Create move-zeroes-to-end.cpp

---
+++ array/sol/move-zeroes-to-end.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = -1;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+    }
+};
