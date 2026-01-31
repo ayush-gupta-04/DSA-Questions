1fcc5203eacf3e7f031fc19441e88365ef658bf4
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-01-31 10:36:32 +0530

    Create move-zeros-to-end.cpp

---
+++ array/sol/move-zeros-to-end.cpp
@@ -0,0 +1,16 @@
+// --------------- CODE -----------------
+// TC = O(N)
+// SC = O(1)
+// a simple partition algorithm
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int i = -1;
+        for (int j = 0; j < nums.size(); j++) {
+            if (nums[j] != 0) {
+                i++;
+                swap(nums[i], nums[j]);
+            }
+        }
+    }
+};
