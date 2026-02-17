de87bf47797a32813140228d6d647a0a7e560724
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-02-17 11:57:01 +0530

    Create temp.java

---
+++ temp.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        System.setOut(new PrintStream(new FileOutputStream("output.txt")));
+        Scanner sc = new Scanner(System.in);
+
+
+        // Code
+        
+
+        
+        // Code End
+        sc.close();
+    }
+}
