317124522b48965b5612e3b7c1849563f80c6467
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-03-06 19:37:00 +0530

    Update calendar-I.java

--- segment-trees/sol/calendar-I.java
+++ segment-trees/sol/calendar-I.java
@@ -35,3 +35,52 @@ class MyCalendar {
         return true;
     }
 }
+
+
+
+
+// -------------------------- METHOD-2 : TreeSet (floor , ceil) -----------------------
+// TC -> logN
+// SC -> N
+// 1. Have a tree-set to have everything in ASC order.
+// 2. for an event :
+//    find it's floor , it's ceiling.
+//    if event does'nt overlapp with any of these , good to go.
+//    otherwise return false;
+
+
+
+class Event{
+    int start;
+    int end;
+    public Event(int s, int e){
+        this.start = s;
+        this.end = e;
+    }
+}
+class MyCalendar {
+    TreeSet<Event> bookings;
+    public MyCalendar() {
+        bookings = new TreeSet<Event>((x , y) -> {
+            if(x.start != y.start) return Integer.compare(x.start , y.start);
+            else return Integer.compare(x.end , y.end);
+        });
+    }
+    
+    public boolean book(int s, int e) {
+        Event eve = new Event(s,e);
+        Event floorEve = bookings.floor(eve);
+        Event ceilingEve = bookings.ceiling(eve);
+        if(floorEve != null){
+            if(overlap(eve,floorEve)) return false;
+        }
+        if(ceilingEve != null){
+            if(overlap(eve,ceilingEve)) return false;
+        }
+        bookings.add(eve);
+        return true;
+    }
+    public boolean overlap(Event e1 , Event e2){
+        return (!(e2.end <= e1.start || e2.start >= e1.end));
+    }
+}
