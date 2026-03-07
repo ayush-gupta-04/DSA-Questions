26ecc4959c8b410568c35531894a194c4866cdaa
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-03-07 11:25:10 +0530

    Update number-of-longest-increasing-subsequences.java

--- dp/sol/number-of-longest-increasing-subsequences.java
+++ dp/sol/number-of-longest-increasing-subsequences.java
@@ -1,5 +1,17 @@
-// use the concept of .. number of ways to arrive at the destination.
-
+// ------------------------------------ BOTTOM-UP DP ------------------------------------------------------
+// TC -> O(N^2)
+// SC -> O(N)
+// Normally, we solve the Number of LIS using a $O(N^2)$ DP approach. 
+// We maintain two arrays for an input array nums:
+// dp[i]: The length of the LIS ending at index i.
+// count[i]: The number of LIS ending at index i.
+// The Logic:
+// For every element nums[i], we look back at all previous elements nums[j] (where j < i).
+// If nums[j] < nums[i] (meaning we can extend the subsequence):
+// 1. If dp[j] + 1 > dp[i]: We found a strictly longer subsequence.
+//    We update dp[i] = dp[j] + 1 and carry over the count: count[i] = count[j].
+// 2. If dp[j] + 1 == dp[i]: We found another way to make the same longest subsequence. 
+//    We add the counts together: count[i] = count[i] + count[j].
 class Solution {
     public int findNumberOfLIS(int[] nums) {
         int n = nums.length;
