5bb6d4034863e04d9c3ef59eee5f6dcc3e253f8e
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-03-11 16:55:27 +0530

    Update move-zeros-to-end.cpp

--- array/sol/move-zeros-to-end.cpp
+++ array/sol/move-zeros-to-end.cpp
@@ -1,4 +1,4 @@
-// --------------- CODE -----------------
+// --------------- METHOD -1 : Sub-Optimal -----------------
 // TC = O(N)
 // SC = O(1)
 // a simple partition algorithm
@@ -10,6 +10,41 @@ public:
             if (nums[j] != 0) {
                 i++;
                 swap(nums[i], nums[j]);
+            }
+        }
+    }
+};
+
+
+// ------------- METHOD - 2 : Optimal ---------------------
+// TC -> N
+// SC -> 1
+// start with
+// j -> At first zero.
+// i -> j + 1.
+class Solution {
+public:
+    // Function to move zeroes to the end
+    void moveZeroes(vector<int>& nums) {
+        // Pointer to the first zero
+        int j = -1;
+
+        // Find the first zero
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 0) {
+                j = i;
+                break;
+            }
+        }
+
+        // If no zero found, return
+        if (j == -1) return;
+
+        // Start from the next index of first zero
+        for (int i = j + 1; i < nums.size(); i++) {
+            if (nums[i] != 0) {
+                swap(nums[i], nums[j]);
+                j++;
             }
         }
     }
