6f799727b60ebd58ca8ad9c96214484c0eabb9d3
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-04-06 15:18:09 +0530

    Update temp.java

--- temp.java
+++ temp.java
@@ -1,3 +1,6 @@
+// Taking input from input.txt
+// Giving output int output.txt
+// Using Scanner
 import java.io.*;
 import java.util.*;
 
@@ -15,5 +18,50 @@ public class Main {
         
         // Code End
         sc.close();
+    }
+}
+
+
+
+
+// Taking input from input.txt
+// Giving output int output.txt
+// Using bufferReader ...
+// &
+// CP template
+
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    static BufferedReader br;
+    static StringTokenizer st;
+    static PrintWriter out;
+    static boolean ONLINE_JUDGE = true;
+
+    public static void main(String[] args) throws Exception {
+        if(ONLINE_JUDGE){
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }else{
+            br = new BufferedReader(new FileReader("input.txt"));
+            out = new PrintWriter(new FileWriter("output.txt"));
+        }
+        
+        // CODE
+
+        
+        
+
+
+        // -------------
+        
+        if(ONLINE_JUDGE){
+            br.close();
+        }else{
+            out.flush();
+            out.close();
+            br.close();
+        }
     }
 }
