f7ec24a7e0c81aa7872546e543ac3e39d512cbba
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-11 21:02:25 +0530

    Create number-of-substrings-containing-all-three-characters.java

---
+++ sliding-window/sol/number-of-substrings-containing-all-three-characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int numberOfSubstrings(String s) {
+        int a = -1;
+        int b = -1;
+        int c = -1;
+        int i = 0;
+        int count =0;
+        while(i < s.length()){
+            if(s.charAt(i) == 'a'){
+                a = i;
+            }else if(s.charAt(i) == 'b'){
+                b = i;
+            }else{
+                c = i;
+            }
+            int min = Math.min(a , Math.min(b , c));
+            if(min != -1){
+                count = count + min + 1;
+            }
+            i++;
+        }
+        return count;
+    }
+}
