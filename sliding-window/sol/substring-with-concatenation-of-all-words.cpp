cfc2e69bf853409461f6c94b2c150e7b31c8e8d6
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-12 17:51:05 +0530

    Update substring-with-concatenation-of-all-words.cpp

--- sliding-window/sol/substring-with-concatenation-of-all-words.cpp
+++ sliding-window/sol/substring-with-concatenation-of-all-words.cpp
@@ -1,48 +1,54 @@
 // this is a classic sliding window problem.
-// we know that word length is same .. therefore move by k.
+// we know that word length is same .. therefore we start from 0 to k-1 .. so that we can get to the starting point of every word.
 // algo :
-// -> if a word is unknown .. reset the map .. reset the window.
-// -> if a word sub is known .. add it to the map.
-// -> if freq of this word sub is greater then we want .. shrink the window until its valid again.
+// -> if a word at r is unknown .. reset the map .. reset the window.
+// -> if a word at r (sub) is known .. add it to the map.
+// -> if freq of this word sub is greater than we want .. shrink the window from left until its valid again.
 // -> after this .. just check if the l and r covers the required length or not... if yes then push l to res.
+
+
 class Solution {
-public:
-    vector<int> findSubstring(string s, vector<string>& words) {
-        map<string , int> mainMap;
-        for(auto& w : words){
-            mainMap[w]++;
+    public List<Integer> findSubstring(String s, String[] words) {
+        Map<String, Integer> mainMap = new HashMap<>();
+        for (String w : words) {
+            mainMap.put(w, mainMap.getOrDefault(w, 0) + 1);
         }
 
-        int n = s.size();
-        int m = words.size();
-        int k = words[0].size();
+        int n = s.length();
+        int m = words.length;
+        int k = words[0].length();
 
-        int i= 0;
-        vector<int> res;
-        while(i < k){
+        int i = 0;
+        List<Integer> res = new ArrayList<>();
+        
+        while (i < k) {
             int l = i;
             int r = i;
-            map<string,int> map;
+            Map<String, Integer> map = new HashMap<>();
 
-            while(r <= n - k){
-                string sub = s.substr(r,k);
-                if(mainMap.find(sub) == mainMap.end()){
-                    // reset map;
+            while (r + k <= n) {
+                String sub = s.substring(r, r + k);
+                
+                // in this case we have to remove this sub from the whole window ..
+                // so we reset the map and move the r and l to after sub.
+                if (!mainMap.containsKey(sub)) {
                     map.clear();
                     r += k;
                     l = r;
                     continue;
                 }
-                map[sub]++;
-                while(l <= r && map[sub] > mainMap[sub]){
-                    string leftSub = s.substr(l,k);
-                    map[leftSub]--;
-                    if(map[leftSub] == 0) map.erase(leftSub);
+                
+                map.put(sub, map.getOrDefault(sub, 0) + 1);
+                
+                while (l <= r && map.get(sub) > mainMap.get(sub)) {
+                    String leftSub = s.substring(l, l + k);
+                    map.put(leftSub, map.get(leftSub) - 1);
+                    if (map.get(leftSub) == 0) map.remove(leftSub);
                     l += k;
                 }
 
-                if(r+k - l == (k*m)){
-                    res.push_back(l);
+                if (r + k - l == (k * m)) {
+                    res.add(l);
                 }
                 r += k;
             }
@@ -50,4 +56,4 @@ public:
         }
         return res;
     }
-};
+}
