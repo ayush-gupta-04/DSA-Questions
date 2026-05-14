94041b65a8ed16d10fd26610eea79e66da0239ec
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-14 23:23:02 +0530

    Update N-Queens.java

--- backtracking/sol/N-Queens.java
+++ backtracking/sol/N-Queens.java
@@ -1,3 +1,10 @@
+// ------------------ Brute-Force ------------------
+// Time : (N!*N) 
+//   - we try all possible permutations of placing the queens and check for safety.
+// Space : N^2 + N
+//   - N^2 -> for storing ans.
+//   - N -> for stack space.
+
 class Solution {
     List<List<String>> ans = new ArrayList<>();
     private boolean isSafePlace(int n, char[][] nQueens, int row, int col) {
@@ -29,7 +36,7 @@ class Solution {
         return true;
     }
     public void nQueens(char[][] grid, int row,int n){
-        if(n == 0){
+        if(row == grid.length){
             List<String> list = new ArrayList<>();
             for(char[] col : grid){
                 list.add(new String(col));
@@ -37,19 +44,15 @@ class Solution {
             ans.add(list);
             return;
         }
-        if(row == grid.length){
-            return;
-        }
-        for(int i = 0; i < grid.length; i++){
-            if(isSafePlace(grid.length,grid,row,i)){
-                grid[row][i] = 'Q';
-                nQueens(grid,row + 1,n - 1);
-                grid[row][i] = '.';
+
+        for(int col = 0; col < grid[0].length; col++){
+            if(isSafePlace(grid.length,grid,row,col)){
+                grid[row][col] = 'Q';
+                nQueens(grid,row + 1,n - 1);   // go to the next row.
+                grid[row][col] = '.';
             }
         }
 
-
-        //if column out of bound --> we can place no queen in this col...hence return.
         return;
     }
     public List<List<String>> solveNQueens(int n) {
@@ -58,6 +61,74 @@ class Solution {
             Arrays.fill(col,'.');
         }
         nQueens(grid,0,n);
+        return ans;
+    }
+}
+
+
+
+
+// ------------------ Optimal Approach : Using an extra Hash Array --------------------
+
+// It is very inefficient to check the whole col , diagonal.
+// we use a precomputed hash array for col,upperRight diagonal , upperLeft Diagonal.
+// col[] : 
+// - size -> n.
+// - access -> col.
+
+// upperRight[] :
+// - size -> 2*n - 1.
+// - access -> row + col
+
+// upperLeft[] :
+// - size -> 2*n - 1.
+// - access -> (n-1) + (col-row)
+
+// Time Complexity: (N!)
+//   - N! -> we try all possible permutations of placing the queens.
+// Space Complexity: N^2 + 4N
+//   - N^2 : to store ans.
+//   - 4N : 3 for hash array ... 1 for stack space
+class Solution {
+    List<List<String>> ans = new ArrayList<>();
+    public void nQueens(int N,char[][] grid, int row,int n , boolean[] column , boolean[] upperRight , boolean[] upperLeft){
+        if(row == N){
+            List<String> list = new ArrayList<>();
+            for(char[] col : grid){
+                list.add(new String(col));
+            }
+            ans.add(list);
+            return;
+        }
+
+        for(int col = 0; col < N; col++){
+            if(column[col] || upperRight[row+col] || upperLeft[N-1 + (col-row)]) continue;
+
+            grid[row][col] = 'Q';
+            column[col] = true;
+            upperRight[row + col] = true;
+            upperLeft[N-1 + (col-row)] = true;
+
+            nQueens(N,grid,row + 1,n - 1,column,upperRight,upperLeft);   // go to the next row.
+            
+            grid[row][col] = '.';
+            column[col] = false;
+            upperRight[row + col] = false;
+            upperLeft[N-1 + (col-row)] = false;
+        }
+
+        return;
+    }
+    public List<List<String>> solveNQueens(int n) {
+        char[][] grid = new char[n][n];
+        for(char[] col : grid){
+            Arrays.fill(col,'.');
+        }
+
+        boolean[] column = new boolean[n];
+        boolean[] upperRight = new boolean[2*n - 1];
+        boolean[] upperLeft = new boolean[2*n - 1];
+        nQueens(grid.length,grid,0,n,column,upperRight,upperLeft);
         return ans;
     }
 }
