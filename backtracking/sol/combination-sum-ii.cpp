0b96c0285bed7eacc88611f3fac2915d720df10d
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-14 22:20:54 +0530

    Update combination-sum-ii.cpp

--- backtracking/sol/combination-sum-ii.cpp
+++ backtracking/sol/combination-sum-ii.cpp
@@ -1,36 +1,39 @@
+// Time Complexity: O(2n * k)
+//   - For each of the 2n subsequences, storing takes O(k) time where k is the average length of each combination.
+// Space Complexity: O(k * x)
+//   - To store all x valid combinations, each of average length k.
+
+import java.util.*;
+
 class Solution {
-public:
-    vector<vector<int>> res;
-    void fun(vector<int>& nums ,int target , int i,vector<int>& list){
+    List<List<Integer>> res = new ArrayList<>();
+    void fun(int[] nums,int target,int i,List<Integer> list){
         if(target == 0){
-            res.push_back(list);
+            res.add(new ArrayList<>(list));
             return;
         }
-        if(i == nums.size()){
+        if(i == nums.length){
             return;
         }
         // take;
         if(target >= nums[i]){
-            list.push_back(nums[i]);
-            fun(nums, target - nums[i],i + 1 , list);
-            list.pop_back();
+            list.add(nums[i]);
+            fun(nums,target - nums[i],i + 1,list); // take the element only once.
+            list.remove(list.size() - 1);
         }
-
         // not - take ... then don't any instance of it.
-
         int j = i;
         j++;
-        while(j < nums.size() && nums[j] == nums[j-1]){
+        while(j < nums.length && nums[j] == nums[j - 1]){
             j++;
         }
-
-        fun(nums , target , j, list);
-        
+        fun(nums,target,j,list);
     }
-    vector<vector<int>> combinationSum2(vector<int>& nums, int target) {
-        vector<int> list;
-        sort(nums.begin() , nums.end());
-        fun(nums, target , 0 , list);
+
+    public List<List<Integer>> combinationSum2(int[] nums,int target) {
+        List<Integer> list = new ArrayList<>();
+        Arrays.sort(nums);
+        fun(nums,target,0,list);
         return res;
     }
-};
+}
