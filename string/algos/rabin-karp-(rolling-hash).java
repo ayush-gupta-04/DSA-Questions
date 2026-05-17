572c26b324876a77989dd484e5f10904d4668eac
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-17 17:10:12 +0530

    Update rabin-karp-(rolling-hash).java

--- string/algos/rabin-karp-(rolling-hash).java
+++ string/algos/rabin-karp-(rolling-hash).java
@@ -1,3 +1,49 @@
+// Rabin-Karp-Algorithm : Standard Algo.
+// Formula for Hash : 
+//     - hash of ABC = A*(p^2) + B*(p^1) + C*(p^0)
+
+// What we do here ?
+// If we have a window size K ... then we precalculate POW = p^(K-1) % mod
+// We calculate the first window .. then we slide.
+// we slide a window .. remove the old char and add the new char to the hash of the string.
+
+// How to update the Hash for the next window : 
+// POW = p^(K-1) % mod
+// hash = (hash - oldChar*POW)%MOD;   // remove the old char.
+// hash = (hash*p + newChar)%MOD;     // add the new char.
+  
+
+
+
+    
+
+
+
+
+
+
+
+// Rabin-Karp-Algorithm : CP Algorithm.
+// Formula for hash : 
+//     - hash of ABC = A*(p^0) + B*(p^1) + C*(p^2)
+
+// What we do here ?
+// - precomputes the powers of p to the size of txt.
+// - build the prefixHash of txt.
+
+// - hash[i:j] -> hash of substring from i to j will be
+//             -> (prefixHash[j] - prefixHash[i-1])/(p^i)
+//             -> we never divide by (p^i) . . . instead we compare by multiplication.
+
+// Pros and Cons : 
+// Cons : 
+// - not standard.
+// - we never get the real hash of the substring.
+// - we use 2 extra arrays ... one for powers of p ... next for prefixHash.
+// Pros : 
+// - Use to solve harder problems.
+// - to compare any substring it just takes O(1) time.
+
 import java.io.*;
 import java.util.*;
 
@@ -9,7 +55,7 @@ public class Main {
         int s = str.length();
         int t = txt.length();
         long p = 31;
-        long MOD = (int)1e9+9;
+        long MOD = 1_000_000_009L;
         List<Integer> ans = new ArrayList<>();
         
 
