a543e9bb5971ce041dba66a60487615901b95434
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-05-27 21:37:19 +0530

    Update subarray-sum-equals-k.java

--- array/sol/subarray-sum-equals-k.java
+++ array/sol/subarray-sum-equals-k.java
@@ -1,3 +1,35 @@
+// ---------------- Optimal Approach : Only Positives | Positives + Zeros ------------------------
+// time : 2*N
+// space : 1
+
+// Here we need to count subarray with a property K
+// we can change the equality and count with sliding window approach.
+class Solution {
+    int fun(int[] nums, int k){
+        int n = nums.length;
+        int cnt = 0;
+        int sum = 0;
+        int r = 0;
+        int l = 0;
+        while(r < n){
+            sum += nums[r];
+            while(l<=r && sum > k){
+                sum -= nums[l];
+                l++;
+            }
+            
+            cnt += (r - l + 1);
+            r++;
+        }
+        return cnt;
+    }
+    public int subarraySum(int[] nums, int k) {
+        return fun(nums,k) - fun(nums,k-1);
+    }
+}
+
+
+// --------------------- Optimal Approach : Every Integer -------------------------
 // time : N
 // space : N
 
