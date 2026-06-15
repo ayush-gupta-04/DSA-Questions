fd099dad42c810cdc8031a8c8f6bf7f09cdc851f
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-06-15 16:05:09 +0530

    Update construct-binary-search-tree-from-preorder-traversal.java

--- binary-search-tree/sol/construct-binary-search-tree-from-preorder-traversal.java
+++ binary-search-tree/sol/construct-binary-search-tree-from-preorder-traversal.java
@@ -1,3 +1,4 @@
+// --------------------- Brute - force ------------------------
 // time : N*N
 // space : H
 
@@ -26,5 +27,40 @@ class Solution {
     public TreeNode bstFromPreorder(int[] pre) {
         int n = pre.length;
         return fun(pre , 0,n-1);
+    }
+}
+
+
+
+// ----------------------- Better Approach ------------------
+// time : NlogN
+// space : N (inorder array) + N (hash map) + H (recursive)
+
+// - sort the preorder array .. it will become the inorder traversal of the BST.
+// - we have preorder and inorder array.
+// - we can easily make the BT out of it.
+
+
+// ------------------------ Optimal Approach -----------------
+// time : N
+// space : H
+
+
+// we will be assigning a upper bound to every node.
+// node -to-> left ... (bound for left will be node.val)(everyting will be less than node.val on left).
+// node -to-> right ... (bound for right will be bound)(everyting will be less than bound on right).
+// we have taken a global increasing idx.
+class Solution {
+    TreeNode fun(int[] pre , int bound , int[] idx){
+       if(idx[0] == pre.length || pre[idx[0]] > bound) return null;
+
+       TreeNode node = new TreeNode(pre[idx[0]]);
+       idx[0]++;
+       node.left = fun(pre , node.val , idx);
+       node.right = fun(pre , bound , idx);
+       return node;
+    }
+    public TreeNode bstFromPreorder(int[] pre) {
+        return fun(pre , Integer.MAX_VALUE , new int[]{0});
     }
 }
