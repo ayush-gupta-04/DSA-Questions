022c212237c373f1e6435b4a707a49f82d18b922
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-07-15 16:56:11 +0530

    Create division-without-using-multiplication-division-and-mod-operator.java

---
+++ bit-manipulation/sol/division-without-using-multiplication-division-and-mod-operator.java
@@ -0,0 +1,32 @@
+// time : logN
+// space : 1
+
+class Solution {
+    public int divide(int A, int B) {
+        if(A == B) return 1;
+        if (A == Integer.MIN_VALUE && B == -1) return Integer.MAX_VALUE;
+        if (B == 1) return A;
+            
+
+        // The sign will be negative only if sign of 
+        // divisor and dividend are different
+        int sign = ((A < 0) ^ (B < 0)) ? -1 : 1;
+
+        long a = Math.abs((long)A);
+        long b = Math.abs((long)B);
+
+        int q = 0;
+
+
+        for (int i = 31; i >= 0; --i) {
+
+            // Check if (divisor << i) <= dividend
+            if ((b << i) <= a) {
+                a -= (b << i);
+                q |= (1 << i);
+            }
+        }
+
+        return sign * q;
+    }
+}
