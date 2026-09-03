28a24d6c311477320b61446506b5bdd420e7871d
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-09-03 22:54:05 +0530

    Update first-negative-integer-in-every-window-of-size-k.java

--- stack-n-queue/sol/first-negative-integer-in-every-window-of-size-k.java
+++ stack-n-queue/sol/first-negative-integer-in-every-window-of-size-k.java
@@ -1,8 +1,4 @@
-// we are maintaining clusters in deque.
-// cluster of (----)(++0+0)
-// cluster of (-ve numbers)(+ve & 0's)
-// -ve numbers are always in the front.
-// if peekLast has +ve number .. and i am -ve .. i don;t need +ve in the deque...pop +ve
+// we will only put -ve numbers in the deque.
 
 
 class Solution {
@@ -14,15 +10,14 @@ class Solution {
         List<Integer> list = new ArrayList<>();
         
         while(r < n){
-            while(!d.isEmpty() && arr[r] < 0 && arr[d.peekLast()] >= 0){
-                d.pollLast();
+            if(arr[r] < 0){
+                d.offerLast(r);
             }
-            d.offerLast(r);
             
-            if(d.peekLast() - d.peekFirst() + 1 > k) d.pollFirst();
+            if(!d.isEmpty() && r - d.peekFirst() + 1 > k) d.pollFirst();
             
             if(r >= k-1){
-                if(arr[d.peekFirst()] < 0) list.add(arr[d.peekFirst()]);
+                if(!d.isEmpty()) list.add(arr[d.peekFirst()]);
                 else list.add(0);
             }
             r++;
