38a21a79d9f88edd5df96b714ff8c0389ad9d35a
Ayush Gupta <152642477+ayush-gupta-04@users.noreply.github.com>
2026-09-05 13:57:08 +0530

    Update binary-tree-right-side-view.java

--- binary-tree/sol/binary-tree-right-side-view.java
+++ binary-tree/sol/binary-tree-right-side-view.java
@@ -6,43 +6,32 @@
 // for each node just identify if that node is the rightmost in the row.
 // if yes then add it to the ans.
 
-class Data{
-    TreeNode node;
-    int row;
-    int col;
-    public Data(TreeNode node , int row, int col){
-        this.node = node;
-        this.row = row;
-        this.col = col;
-    }
-}
 class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        Deque<Data> q = new ArrayDeque<>();
-        List<Integer> list = new ArrayList<>();
+    public ArrayList<Integer> rightView(Node root) {
+        // code here
+        Deque<Node> q = new ArrayDeque<>();
+        ArrayList<Integer> list = new ArrayList<>();
         if(root ==null) return list;
-
-        q.offerLast(new Data(root,0,0));
-
+        
+        q.offerLast(root);
+        
         while(!q.isEmpty()){
-            TreeNode node = q.peekFirst().node;
-            int row = q.peekFirst().row;
-            int col = q.peekFirst().col;
-            q.pollFirst();
-
-            if(node.left != null) q.offerLast(new Data(node.left , row + 1, col-1));
-            if(node.right != null) q.offerLast(new Data(node.right , row + 1, col+1));
-
-            // check to see if this node is last in the row.
-            if(q.isEmpty() || q.peekFirst().row == row + 1){
-                list.add(node.val);
+            int size = q.size();
+            
+            for(int i = 0; i < size; i++){
+                Node node = q.pollFirst();
+                
+                // add last node of every level to the list;
+                if(i==size-1) list.add(node.data);
+                
+                if(node.left != null) q.offerLast(node.left);
+                if(node.right != null) q.offerLast(node.right);
             }
         }
-
+        
         return list;
     }
 }
-
 
 
 // ---------------- Method 2 : DFS --------------
